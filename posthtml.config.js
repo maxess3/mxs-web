b06724f2d0059b30b983b5e958e557820c92f0ef
maxess3 <maxschell31@gmail.com>
2024-04-04 09:47:59 +0200

    Update url

---
+++ posthtml.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    plugins: {
+      "posthtml-shorten": {
+        shortener: {
+          process: function (url) {
+            return new Promise((resolve, reject) => {
+              resolve(url.replace(".html", ""));
+            });
+          },
+        },
+        tag: ["a"], // Allowed tags for URL shortening
+        attribute: ["href"], // Attributes to replace on the elements
+      },
+    },
+  };
